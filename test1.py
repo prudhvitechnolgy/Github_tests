a7c5f0de523d585264f80f784be01686435d1f48
prudhvitechnolgy <122344235+prudhvitechnolgy@users.noreply.github.com>
2023-02-08 11:10:02 +0530

    game app using functions

--- test1.py
+++ test1.py
@@ -1,32 +1,38 @@
-#Types of functions
-#1.without parameters without return values
-def welcome_msg():
-    print("hello nice to see your!")
-    print('welcome to our application')
-welcome_msg()
-#2.without parameters with return values
-def sum_of_first_10_numbers():
-    sum=0
-    for i in range(1,11):
-        sum=sum+i
-        return sum
-#3.with parameters without return values:
-def welcome_user(name,gender):
-    if gender=='M':
-        print("welcome mr",'have a nice day!')
+game_list=[0,1,2]
+def display_game(game_list):
+    print("Here  is the current list:")
+    print(game_list)
+display_game(game_list)
+def position_choice():
+    choice='wrong'
+    while choice not in ['0','1','2']:
+        choice=input("pick a position (0,1,2):")
+        if choice not in ['0','1','2']:
+            print("soory invalid choice")
+    return int(choice)
+position_choice()
+def replacement_choice(game_list,position):
+    user_placement=input('type a string to place at positon')
+    game_list[position]=user_placement
+    return game_list
+naresh=replacement_choice(game_list,1)
+print(naresh)
+def gameon_choice():
+    choice='wrong'
+    while choice not in ['Y','N']:
+        choice=input("keep playing (Y or N)")
+        if choice not in ['Y','N']:
+            print("sorry i dont understand please choose y or n")
+    if choice=='y':
+        return True
     else:
-        print('welcome mrs','have a nice day!')
-    name=input('enter your name')
-    gender=input('enter your gender(M/F)')
-    welcome_user(name,gender)
-#4.with parameters with return values
-def get_count(arr,size,target_value):
-    cnt=0
-    for i in range(0,size):
-        if arr[i]==target_value:
-            cnt=cnt+1
-    return cnt
-my_list=[1,2,2,3,3,3,4,5,4,4,4,6,7]
-print('1 occurance:',get_count(my_list,len(my_list),1))
-print('2 occurance:',get_count(my_list,len(my_list),2))
-print('3 occurance:',get_count(my_list,len(my_list),3))
+        return False
+gameon_choice()
+game_on=True
+game_list=[0,1,2]
+while game_on:
+    display_game(game_list)
+    position=position_choice()
+    game_list=replacement_choice(game_list,position)
+    display_game(game_list)
+    game_on=gameon_choice()
